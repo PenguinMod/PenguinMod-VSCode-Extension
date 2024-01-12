4f87104d9bf5e23525118f0318b7aeef639ad3f7
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2024-01-12 14:58:09 -0600

    a buncha stuff that happend next

---
+++ index.js
@@ -0,0 +1,43 @@
+const vscode = require("vscode");
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  let disposable = vscode.commands.registerCommand(
+    "extension.penguinmod",
+    () => {
+      // Create and show panel
+      const panel = vscode.window.createWebviewPanel(
+        "pm",
+        "PenguinMod",
+        vscode.ViewColumn.One,
+        {}
+      );
+      const code = new Buffer.from(vscode.window.activeTextEditor.document.getText()).toString("base64");
+      
+      panel.webview.html = `<!DOCTYPE html>
+      <html>
+        <head></head>
+        <body>
+          <iframe width="100%" height="100%"
+            src="https://studio.penguinmod.com/editor.html?livetests">
+            <script>
+              const frame = window.frames[0];
+              frame.window.onload = () => {
+                vm.loadExtensionURL("data:text/plain;base64,${code}")
+              }
+            </script>
+          </body>
+        </html>`
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
